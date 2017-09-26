11857bd82032bcd8191148adfa276e78d8b7afce
Kelly Rankin <kellyr@stcu.org>
2017-09-26 14:19:11 -0700

    Added subscription reference

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, Pipe, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, OnDestroy, Pipe, ChangeDetectionStrategy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as rootIndexReducer from './common/state/index.reducer';
 import * as layoutActions from './common/layout/state/layout.actions';
+import { Observable } from 'rxjs/observable';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
@@ -11,15 +13,20 @@ import * as layoutActions from './common/layout/state/layout.actions';
 export class AppComponent {
   title = 'app';
   public backgroundColor: string = 'white';
+  public backgroundColorSubscription: Subscription;
 
   constructor(
     private store: Store<rootIndexReducer.AppState>
   ) {}
 
   ngOnInit() {
-    this.store.select(rootIndexReducer.getBackgroundColor).subscribe(
+    this.backgroundColorSubscription = this.store.select(rootIndexReducer.getBackgroundColor).subscribe(
       color => this.backgroundColor = color
     );
+  }
+
+  ngOnDestroy() {
+    this.backgroundColorSubscription.unsubscribe();
   }
 
   toggleBackground(color: string) : void {
--- src/app/common/state/index.reducer.ts
+++ src/app/common/state/index.reducer.ts
@@ -16,8 +16,8 @@ export const reducers = {
 const developmentReducer:Function = compose(storeLogger(), combineReducers)(reducers);
 
 export function metaReducer(state: any, action: any) {
-    //return developmentReducer(state, action);
-    return combineReducers(reducers);
+    return developmentReducer(state, action);
+    //return combineReducers(reducers);
 }
 
 export const getLayoutState = (state: AppState) => state.layout;
