8b837da34534f5ba960d584d0981d55ab4085dc5
Kelly Rankin <kellyr@stcu.org>
2017-09-26 17:28:31 -0700

    Fixing color toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,7 +29,7 @@ export class AppComponent {
     this.backgroundColorSubscription.unsubscribe();
   }
 
-  toggleBackground(color: string) : void {
+  public toggleBackground(color: string) : void {
     this.store.dispatch(new layoutActions.SetBackgroundColor(color));
   }
 }
